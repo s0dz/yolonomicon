5ebcb731501c207475dc366f604107afe7e223b8
Garrett McTear <garrett.mctear@gmail.com>
2013-03-17 21:03:22 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yolonomicon::Application.config.secret_token = 'b7aab9f6cbd471941466b925d9f778a6e0028bf598c9ccff5e588fbdd1e7e61a895f483942bec34584d0ceec21052b6eda304b4dd0c72e60c623c70126a10701'
