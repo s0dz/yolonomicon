10912d4083476ba56e5ed92e9bf4aa60b7c06851
Garrett McTear <garrett.mctear@gmail.com>
2013-03-30 18:28:25 -0600

    Finish layout and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
-  has_many :microposts
+  attr_accessible :name, :email
+
+  def initialize(attributes = {})
+  	@name  = attributes[:name]
+  	@email = attributes[:email]
+  end
+
+  def formatted_email
+  	"#{@name} <#{@email}"
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,46 +1,35 @@
 require 'spec_helper'
 
 describe "Static pages" do
-	
-	describe "Home page" do
 
-		it "should have the content 'Yolonomicon'" do
-			visit '/static_pages/home'
-			page.should have_content('Yolonomicon')
-		end
+  subject { page }
 
-		it "should have title 'Home'" do
-			visit '/static_pages/home'
-			page.should have_selector('title',
-				:text => "Yolonomicon | Home")
-		end
-	end
+  describe "Home page" do
+    before { visit root_path }
 
-	describe "Help page" do
+    it { should have_selector('h1',    text: 'Yolonomicon') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
+  end
 
-		it "should have the content 'Help'" do
-			visit '/static_pages/help'
-			page.should have_content('Help')
-		end
+  describe "Help page" do
+    before { visit help_path }
 
-		it "should have the title 'Help'" do
-			visit '/static_pages/help'
-			page.should have_selector('title',
-				:text => "Yolonomicon | Help")
-		end
-	end
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
+  end
 
-	describe "About page" do
+  describe "About page" do
+    before { visit about_path }
 
-		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
-			page.should have_content('About Us')
-		end
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
+  end
 
-		it "should have the title 'About Us'" do
-			visit '/static_pages/about'
-			page.should have_selector('title',
-				:text => "Yolonomicon | About Us")
-		end
-	end
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
+  end
 end
