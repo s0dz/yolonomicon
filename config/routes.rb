86aeb7b94eedc4809ee37f72f63ff0c87dc8eba0
Garrett McTear <garrett.mctear@gmail.com>
2013-04-07 18:56:59 -0600

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Yolonomicon::Application.routes.draw do
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',    to: 'users#new'
+  match '/signin',    to: 'sessions#new'
+  match '/signout',   to: 'sessions#destroy', via: :delete
+
   get "users/new"
 
   get "user/new"
 
   root to: 'static_pages#home'
 
-  match '/signup',  to: 'users#new'
-
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
-
-  resources :microposts
-  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
